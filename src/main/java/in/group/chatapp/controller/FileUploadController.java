41b1ad465551c6d825422ffd65e9a509a3607bd4
RohinR <Rohin>
2025-05-13 03:37:47 +0530

    file upload changes

--- src/main/java/in/group/chatapp/controller/FileUploadController.java
+++ src/main/java/in/group/chatapp/controller/FileUploadController.java
@@ -15,9 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
